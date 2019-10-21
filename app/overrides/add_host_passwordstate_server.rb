a8ad470f3f325713b4f4566596797cf17bb5fdeb
Alexander Olofsson <alexander.olofsson@liu.se>
2019-10-21 14:03:35 +0200

    Remove outdated comment

--- app/overrides/add_host_passwordstate_server.rb
+++ app/overrides/add_host_passwordstate_server.rb
@@ -8,7 +8,6 @@ Deface::Override.new(virtual_path: 'hosts/_operating_system',
                      insert_after: '#root_password',
                      partial:      'foreman_passwordstate/host_password_choice')
 
-# Hostgroup facets are not implemented at the moment
 if Hostgroup.instance_methods.include? :build_passwordstate_facet
   Deface::Override.new(virtual_path:  'hostgroups/_form',
                        name:          'hg_add_passwordstate_server_selection',
