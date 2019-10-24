e56eb390ea1288520fd906f1b3a2a3f0e4d9eec6
Alexander Olofsson <alexander.olofsson@liu.se>
2019-10-24 12:38:46 +0200

    Only load managed passwords on tab click

---
+++ app/assets/javascripts/foreman_passwordstate/host_show_extensions.js
@@ -0,0 +1,8 @@
+function trigger_delayed_ajax(element) {
+  $(element).each(function() {
+    var url = $(this).data('delayed-ajax-url');
+    $(this).removeAttr('data-delayed-ajax-url');
+    $(this).attr('data-ajax-url', url);
+    onContentLoad();
+  });
+}
