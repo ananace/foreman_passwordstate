efec51714eba822fda6f5abeb930782c5bc2e5fa
Alexander Olofsson <alexander.olofsson@liu.se>
2019-10-28 09:15:11 +0100

    Remove reload-on-click "functionality"

--- app/assets/javascripts/foreman_passwordstate/host_show_extensions.js
+++ app/assets/javascripts/foreman_passwordstate/host_show_extensions.js
@@ -1,7 +1,10 @@
 function trigger_delayed_ajax(element) {
   $(element).each(function() {
-    var url = $(this).data('delayed-ajax-url');
-    $(this).attr('data-ajax-url', url);
-    onContentLoad();
+    var url = $(this).attr('data-delayed-ajax-url');
+    if (url) {
+      $(this).removeAttr('data-delayed-ajax-url');
+      $(this).attr('data-ajax-url', url);
+      onContentLoad();
+    }
   });
 }
