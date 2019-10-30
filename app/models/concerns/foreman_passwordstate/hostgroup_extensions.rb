f408eb30b0a6c960b1c54e0cbbfa384dad03326e
Alexander Olofsson <alexander.olofsson@liu.se>
2019-10-30 15:15:55 +0100

    Show inherited values in rendered view

--- app/models/concerns/foreman_passwordstate/hostgroup_extensions.rb
+++ app/models/concerns/foreman_passwordstate/hostgroup_extensions.rb
@@ -12,5 +12,18 @@ module ForemanPasswordstate
 
     delegate :passwordstate_server, to: :passwordstate_facet
     delegate :password_list, to: :passwordstate_facet, prefix: :passwordstate
+
+    def inherited_facet_attributes(facet_config)
+      return super unless facet_config.name == :passwordstate_facet
+      inherited_attributes = send(facet_config.name)&.inherited_attributes || {}
+
+      hostgroup_ancestry_cache.reverse_each do |hostgroup|
+        hg_facet = hostgroup.send(facet_config.name)
+        next unless hg_facet
+        inherited_attributes.merge!(hg_facet.inherited_attributes) { |_, left, right| left || right }
+      end
+
+      inherited_attributes
+    end
   end
 end
