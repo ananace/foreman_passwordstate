fcd510ce376fe5e6657b54954ae917aa09f52740
Alexander Olofsson <alexander.olofsson@liu.se>
2024-01-15 13:01:37 +0100

    Avoid issues with potentially merging nil

--- app/models/foreman_passwordstate/passwordstate_host_facet.rb
+++ app/models/foreman_passwordstate/passwordstate_host_facet.rb
@@ -20,7 +20,7 @@ module ForemanPasswordstate
 
     # FOREMAN-37043
     def self.inherited_attributes(hostgroup, facet_attributes)
-      facet_attributes.merge(super) { |_, left, right| left || right }
+      (facet_attributes || {}).merge(super) { |_, left, right| left || right }
     end
   end
 end
